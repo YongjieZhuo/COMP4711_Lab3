2777119953cfdf73c786572b58e95479d84da3d7
10606 <10606@LAPTOP-QD6JGL4B.lan>
2020-09-27 14:09:42 +0800

    added status and fourth student

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $first->add_email('work','jdoe@mcdonalds.com');
 $first->add_grade(65);
 $first->add_grade(75);
 $first->add_grade(55);
+$first->add_status('status', 'Freshman');
 $students['j123'] = $first;
 
 $second = new Student();
@@ -33,6 +34,7 @@ $second->add_email('work2','albert@physics.mit.edu');
 $second->add_grade(95);
 $second->add_grade(80);
 $second->add_grade(50);
+$second->add_status('status', 'Junior');
 $students['a456'] = $second;
 
 $third = new Student();
@@ -43,7 +45,19 @@ $third->add_email('work1','Yongjie@bcit.ca');
 $third->add_grade(95);
 $third->add_grade(90);
 $third->add_grade(85);
+$third->add_status('status', 'Senior');
 $students['b234'] = $third;
+
+$fourth = new Student();
+$fourth->surname = "aa";
+$fourth->first_name = "bb";
+$fourth->add_email('home','baba@braniacs.com');
+$fourth->add_email('work1','baae@bcit.ca');
+$fourth->add_grade(95);
+$fourth->add_grade(80);
+$fourth->add_grade(80);
+$fourth->add_status('status', 'Senior');
+$students['c234'] = $fourth;
 
 ksort($students); // one of the many sort functions
 foreach($students as $student)
